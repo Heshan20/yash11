7efb2e4e4a5a4164e98c0575601def2cad31bd4a
Heshan20 <90126737+Heshan20@users.noreply.github.com>
2021-09-30 10:22:38 +0530

    Dataking.py
    Updated Dataking

--- plugins/start.py
+++ plugins/start.py
@@ -5,9 +5,9 @@ from pyrogram import Client, Filters, StopPropagation, InlineKeyboardButton, Inl
 async def start(client, message):
     # return
     joinButton = InlineKeyboardMarkup([
-        [InlineKeyboardButton("Channel", url="https://t.me/joinchat/ArkWZG_MBmBjMGM1")],
+        [InlineKeyboardButton("Channel", url="https://t.me/datamaruwa")],
         [InlineKeyboardButton(
-            "Report Bugs 😊", url="https://t.me/YASH1029")]
+            "contact developer😊", url="https://t.me/Dk_king_offcial")]
     ])
     welcomed = f"Hey <b>{message.from_user.first_name}</b>\n/help for More info"
     await message.reply_text(welcomed, reply_markup=joinButton)
