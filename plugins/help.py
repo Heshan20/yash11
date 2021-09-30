893786f2dc4e2d0b1c53f84b42c4500b227ff096
Heshan20 <90126737+Heshan20@users.noreply.github.com>
2021-09-30 11:20:49 +0530

    Update help.py
    Updated Dataking

--- plugins/help.py
+++ plugins/help.py
@@ -3,5 +3,15 @@ from pyrogram import Client, Filters
 
 @Client.on_message(Filters.command(["help"]))
 async def start(client, message):
-    helptxt = f"Currently Only supports Youtube Single  (No playlist) Just Send Youtube Url"
+    helptxt = f"⚙ HOW  TO USE BOT ⚙
+
+
+
+✅First step start bot - Type ➡️ /start.. 
+
+✅Next step Copy your YouTube Url Send bot.. 
+
+✅Next step you choose one a like one to download.. 
+
+"
     await message.reply_text(helptxt)
